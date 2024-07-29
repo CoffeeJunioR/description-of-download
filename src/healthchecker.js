377daf8065c777c2e6a358563957408570b1430b
Denis Savanin <superrr.den@list.ru>
2024-07-29 19:34:32 +0300

    Some fixes

--- src/healthchecker.js
+++ src/healthchecker.js
@@ -1,14 +1,13 @@
-export function checkHealth(heroes) {
-    for (let i = 0; i<heroes.length; i++) {
-        let result = 0;
-        if (heroes[i].health > 50) {
-            result = 'healthy';
-        } else if ((50 > heroes[i].health) && (heroes[i].health > 15)) {
-            result = 'wounded';
-        } else {
-            result = 'critical';
-        };
-        return result;
-    }
+export function checkHealth(hero) {
+    let result = 0;
     
+    if (hero.health >= 50) {
+        result = 'healthy';
+    } else if ((hero.health < 50) && (hero.health >= 15)) {
+        result = 'wounded';
+    } else {
+        result = 'critical';
+    };
+
+    return result;
 }
--- test/test.js
+++ test/test.js
@@ -1,22 +1,6 @@
 import { checkHealth } from "../src/healthChecker";
-
-// test('checkHealth at health: 90', () =>{
-//     const hero = {name: 'Маг', health: 10};
-//     const result = checkHealth(hero);
-//     expect(result).toBe('green')
-// });
-const heroes = [
-    {name: 'Маг', health: 10},
-    {name: 'Робот', health: 90},
-    {name: 'Ведьма', health: 40},
-]
-
-test.each([
-    ['healthy', 10, 'critical'],
-    ['wounded', 40, 'wounded'],
-    ['critical', 90, 'healthy']
-])
-('testing healthchecker function with %s status and %i amount', (status, amount, expected) => {
-    const result = checkHealth(heroes);
-    expect(result).toBe(expected);
+const hero = {name: 'Маг', health: 20};
+test('checkHealth at health: 90', () =>{
+    const result = checkHealth(hero);
+    expect(result).toBe('wounded')
 });
